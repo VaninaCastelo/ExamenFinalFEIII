7b5f46e7adbc1499567ccc0e0150ea6c6bb21db9
Vanina Castelo <v.i.castelo@gmail.com>
2023-09-14 17:59:50 -0700

    Agrero cambio de tema

--- ctd-esp-fe3-final/src/App.jsx
+++ ctd-esp-fe3-final/src/App.jsx
@@ -5,14 +5,19 @@ import Navbar from './Components/Navbar'
 import Home from './Routes/Home'
 import Detail from './Routes/Detail'
 import Favs from './Routes/Favs'
-import Contact from './Routes/Contact';
+import Contact from './Routes/Contact'
+import { useGlobalContext } from './Components/utils/global.context';
+
 
 
 function App() {
+  const {state} = useGlobalContext();
+  const {theme} = state;
+  const changeDarkTheme = theme ? 'light' : 'dark';
+    
   return (
-      <div className="App">
+      <div className={changeDarkTheme}>
           <Navbar/>
-          
           <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/dentist/:id' element={<Detail/>}/>
--- ctd-esp-fe3-final/src/Components/Navbar.jsx
+++ ctd-esp-fe3-final/src/Components/Navbar.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useGlobalContext } from './utils/global.context'
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Navbar = () => {
+  
+  const {dispatch} = useGlobalContext();
+
+  const changeDark=() => { 
+    dispatch({type: 'SWITCH_THEME'});
+
+  };
 
   return (
     <nav>
@@ -12,7 +20,7 @@ const Navbar = () => {
       <Link to='/favs'>Favorito</Link>
       <Link to='/form'>Contacto</Link>
       {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
-      <button>Change theme</button>
+      <button onClick={changeDark}>🌙</button>
     </nav>
   )
 }
