2ccaf194e0b6c8760a833f33512059eb9a52b880
Vanina Castelo <v.i.castelo@gmail.com>
2023-09-15 21:30:49 -0700

    Agrego Detalles Footer

--- ctd-esp-fe3-final/src/Components/Footer.jsx
+++ ctd-esp-fe3-final/src/Components/Footer.jsx
@@ -4,15 +4,18 @@ import '../styles/footer.css';
 const Footer = () => {
   return (
     <footer className="footer">
-    <section><li className="social-icons">
+    <li>
+        <section className="social-icons">
         <a href="URL_DE_FACEBOOK" target="_blank"><img src="../../public/images/ico-facebook.png" alt="Facebook"/></a>
         <a href="URL_DE_INSTAGRAM" target="_blank"><img src="../../public/images/ico-instagram.png" alt="Twitter" /></a>
         <a href="URL_DE_TIKTOK" target="_blank"><img src="../../public/images/ico-tiktok.png" alt="Instagram" /></a>
         <a href="URL_DE_WHATSAPP" target="_blank"><img src="../../public/images/ico-whatsapp.png" alt="Instagram" /></a>
-    </li></section>
-    <section className="logo"><li><p>Powered by</p>
-      <img  src="./images/DH.png" alt='DH-logo' />
-    </li></section>
+        </section>
+        <p>Powered by</p>
+        <img  className="logo-dh" src="./images/DH.png" alt='DH-logo' />
+   
+    </li>
+   
     </footer>
   )
 }
