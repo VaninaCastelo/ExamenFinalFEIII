3bececd2790445d69c6711542289d73c7c3f1784
Vanina Castelo <v.i.castelo@gmail.com>
2023-09-15 20:51:33 -0700

    Agrego CSS Footer

--- ctd-esp-fe3-final/src/Components/Footer.jsx
+++ ctd-esp-fe3-final/src/Components/Footer.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
-//import '../styles/footer.css';
+import '../styles/footer.css';
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <li className="social-icons">
+    <section><li className="social-icons">
         <a href="URL_DE_FACEBOOK" target="_blank"><img src="../../public/images/ico-facebook.png" alt="Facebook"/></a>
         <a href="URL_DE_INSTAGRAM" target="_blank"><img src="../../public/images/ico-instagram.png" alt="Twitter" /></a>
         <a href="URL_DE_TIKTOK" target="_blank"><img src="../../public/images/ico-tiktok.png" alt="Instagram" /></a>
         <a href="URL_DE_WHATSAPP" target="_blank"><img src="../../public/images/ico-whatsapp.png" alt="Instagram" /></a>
-    </li>
-    <li><p>Powered by</p>
-        <img src="./images/DH.png" alt='DH-logo' />
-    </li>
+    </li></section>
+    <section className="logo"><li><p>Powered by</p>
+      <img  src="./images/DH.png" alt='DH-logo' />
+    </li></section>
     </footer>
   )
 }
